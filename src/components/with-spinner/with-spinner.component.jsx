f1a3f3ac2c006caf0b039f5efc671223c8b24f7d
Roman Kolerov <coder8080@mail.ru>
2022-03-24 18:51:09 +0300

    Added spinner styles

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,8 +1,14 @@
 import "./with-spinner.styles.scss"
 
-const Spinner =
+const Spinner = () => (
+  <div className="spinner-container">
+    <div className="spinner"></div>
+  </div>
+)
+
+const withSpinner =
   (WrappedComponent) =>
   ({ isLoading, ...otherProps }) =>
-    isLoading ? <div>spinner</div> : <WrappedComponent {...otherProps} />
+    isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />
 
-export default Spinner
+export default withSpinner
