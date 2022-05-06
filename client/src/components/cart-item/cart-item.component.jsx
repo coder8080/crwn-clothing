d82673ebd787e3d905263ebf29138a0590cfcf28
Roman Kolerov <coder8080@mail.ru>
2022-05-06 17:21:17 +0300

    More optimizations && adding gzip support to express server

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react'
 import './cart-item.styles.scss'
 
 const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
@@ -12,4 +13,4 @@ const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
   </div>
 )
 
-export default CartItem
+export default memo(CartItem)
